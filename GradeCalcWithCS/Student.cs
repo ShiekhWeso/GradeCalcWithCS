2801b9a90f7bfb64086ca2af39de4272b4ce64f3
Weso <mohamed.hosam.eldeeen@gmail.com>
2025-10-14 00:03:37 +0300

    BIG files update
    -Changing a lot of files while working on trying to make the WPF app work

---
+++ GradeCalcWithCS/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeCalcWithCS
+{
+    public class Student
+    {
+        public  string Name { get; set; } = string.Empty;
+        public List<Subject> Subjects { get; set; } = new List<Subject>();
+
+        public double GetTotalPercentage()
+        {
+            double totalMarks = 0;
+            double totalCredits = 0;
+
+            foreach (var subject in Subjects)
+            {
+                totalMarks += subject.Mark;
+                totalCredits += subject.CreditHours;
+            }
+
+            return totalCredits > 0 ? totalMarks / totalCredits : 0;
+        }
+        public double GetGPA()
+        {
+            double totalPoints = 0;
+            double totalCredits = 0;
+
+            foreach (var subject in Subjects)
+            {
+                double gpa = subject.GetGPAvalue();
+                totalPoints += gpa * subject.CreditHours;
+                totalCredits += subject.CreditHours;
+            }
+
+            return totalCredits > 0 ? totalPoints / totalCredits : 0;
+
+        }
+    }
+}
---
+++ GradeCalcWithCS/StudentManager.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Text.Json;
+using GradeCalcWithCS;
+
+namespace GradeCalcWithCS
+{
+    public static class StudentManager
+    {
+        public static List<Student> LoadStudents(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                return JsonSerializer.Deserialize<List<Student>>(json) ?? new List<Student>();
+            }
+            return new List<Student>();
+        }
+
+        public static void SaveStudents(List<Student> students, string filePath)
+        {
+            string json = JsonSerializer.Serialize(students, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(filePath, json);
+        }
+    }
+}
